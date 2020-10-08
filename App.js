20ba6a73e968d70e440f4659ee1d5075e35300db
IanChak <60276491+IanTimchak@users.noreply.github.com>
2020-10-07 21:25:30 -0400

    added assets and updated else

--- App.js
+++ App.js
@@ -5,110 +5,162 @@
  * @format
  * @flow strict-local
  */
-
-import React from 'react';
+import 'react-native-gesture-handler';
+import React, {useRef, useEffect} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
   ScrollView,
   View,
   Text,
-  StatusBar,
+  StatusBar, 
+  Animated,
+  Button,
+  TouchableOpacity, 
+  Alert
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
+
+
+const Stack = createStackNavigator();
+
+const App = () /*=> React$Node = ()*/ => {
+  const fadeAnim = useRef(new Animated.Value(0)).current
+  const fadeText = useRef(new Animated.Value(0)).current
+
+  Animated.timing( //main background
+    fadeAnim,
+     {
+       toValue: 1,
+       delay: 500,
+       duration: 1800,
+       useNativeDriver: true
+     }
+   ).start()
+
+   Animated.timing( //text and buttons
+    fadeText,
+     {
+       toValue: 1,
+       delay: 2450,
+       duration: 800,
+       useNativeDriver: true
+     }
+   ).start()
+
+   const WelcomeScreen = ({navigation}) => {
+    return (
+      <>
+        <StatusBar barStyle="dark-content" />
+          <View style={styles.mainContainer}>
+            <View style={styles.background}>
+              <Animated.Image source = {require('./assets/Aqusafehome.png')} style={{position: 'absolute', width: '100%', height: '100%', opacity: fadeAnim,}}/>
+              <Animated.Text 
+                style={{
+                  fontSize: 60, 
+                  fontWeight: '700', 
+                  paddingTop: 300, 
+                  color: appColors.bleach,
+                  textShadowColor: appColors.darkAccent,
+                  textShadowOffset: {width: 5, height: 3},
+                  textShadowRadius: 0, 
+                  opacity: fadeText}}>
+                AquaSafe
+              </Animated.Text>
             </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
+            <View style={styles.textBlock}>
+              <Animated.View 
+                  style={{
+                    opacity: fadeText,
+                    paddingTop: 600
+                  }}>
+                    <TouchableOpacity style={styles.buttonBlock} 
+                      onPress={() => 
+                        navigation.navigate('Registration')
+                      }>
+                      <Text style={styles.textBlock}>Sign in</Text>
+                    </TouchableOpacity>
+              </Animated.View>
             </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
           </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
+      </>
+      );
+   }
+
+   const LoginScreen = () => {
+     return(
+       <Text>This stack is still being developed. Login is currently disabled</Text>
+     )
+   }
+
+   return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen //Welcome Screen
+          name="Welcome"
+          component={WelcomeScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen //sign in/up
+          name="Registration"
+          component={LoginScreen}
+          options={{title: 'Login or Register'}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+    );
 };
 
+
+
+const appColors = {
+  background: '#85AEF1',
+  darkAccent: '#525C82',
+  titleDark: '#2E3652',
+  titleBright: '#F5F7FF',
+  bleach: '#FFFFFF'
+}
+
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  mainContainer: {
+    flex: 1,
+    alignItems: 'center',
+    height: '100%',
+    justifyContent: 'center',
   },
-  engine: {
+  background: {
+    backgroundColor: '#385BA6',
+    alignItems: 'center',
+    justifyContent: 'center',
     position: 'absolute',
-    right: 0,
+    left: 0,
+    top: 0,
+    width: '100%',
+    height: '100%'
   },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
+  textBlock: {
+    fontSize: 25,
     fontWeight: '400',
-    color: Colors.dark,
+    textShadowColor: appColors.darkAccent,
+    textShadowOffset: {width: 3, height: 3},
+    textShadowRadius: 4,
+    alignContent: 'center',
+    justifyContent: 'center',
+
+    color: appColors.bleach,
   },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+  buttonBlock: {
+    fontSize: 25,
+    fontWeight: '300',
+    height: 60,
+    alignContent: 'center',
+    justifyContent: 'center',
+
+    color: appColors.bleach,
+  }
 });
 
 export default App;
