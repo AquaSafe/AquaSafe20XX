93088bad106f38765faa40fa51e6359af03fe2a0
Alex <alex@2a03.party>
2020-10-07 22:09:46 -0400

    Jesus fuck so many errors
    Took 5 minutes

--- App.js
+++ App.js
@@ -13,90 +13,100 @@ import {
   ScrollView,
   View,
   Text,
-  StatusBar, 
+  StatusBar,
   Animated,
   Button,
-  TouchableOpacity, 
-  Alert
+  TouchableOpacity,
+  Alert,
 } from 'react-native';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
-
-
 const Stack = createStackNavigator();
 
 const App = () /*=> React$Node = ()*/ => {
-  const fadeAnim = useRef(new Animated.Value(0)).current
-  const fadeText = useRef(new Animated.Value(0)).current
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+  const fadeText = useRef(new Animated.Value(0)).current;
 
-  Animated.timing( //main background
+  Animated.timing(
+    //main background
     fadeAnim,
-     {
-       toValue: 1,
-       delay: 500,
-       duration: 1800,
-       useNativeDriver: true
-     }
-   ).start()
+    {
+      toValue: 1,
+      delay: 500,
+      duration: 1800,
+      useNativeDriver: true,
+    },
+  ).start();
 
-   Animated.timing( //text and buttons
+  Animated.timing(
+    //text and buttons
     fadeText,
-     {
-       toValue: 1,
-       delay: 2450,
-       duration: 800,
-       useNativeDriver: true
-     }
-   ).start()
+    {
+      toValue: 1,
+      delay: 2450,
+      duration: 800,
+      useNativeDriver: true,
+    },
+  ).start();
 
-   const WelcomeScreen = ({navigation}) => {
+  const WelcomeScreen = ({navigation}) => {
     return (
       <>
         <StatusBar barStyle="dark-content" />
-          <View style={styles.mainContainer}>
-            <View style={styles.background}>
-              <Animated.Image source = {require('./assets/Aqusafehome.png')} style={{position: 'absolute', width: '100%', height: '100%', opacity: fadeAnim,}}/>
-              <Animated.Text 
-                style={{
-                  fontSize: 60, 
-                  fontWeight: '700', 
-                  paddingTop: 300, 
-                  color: appColors.bleach,
-                  textShadowColor: appColors.darkAccent,
-                  textShadowOffset: {width: 5, height: 3},
-                  textShadowRadius: 0, 
-                  opacity: fadeText}}>
-                AquaSafe
-              </Animated.Text>
-            </View>
-            <View style={styles.textBlock}>
-              <Animated.View 
-                  style={{
-                    opacity: fadeText,
-                    paddingTop: 600
-                  }}>
-                    <TouchableOpacity style={styles.buttonBlock} 
-                      onPress={() => 
-                        navigation.navigate('Registration')
-                      }>
-                      <Text style={styles.textBlock}>Sign in</Text>
-                    </TouchableOpacity>
-              </Animated.View>
-            </View>
+        <View style={styles.mainContainer}>
+          <View style={styles.background}>
+            <Animated.Image
+              source={require('./assets/Aqusafehome.png')}
+              style={{
+                position: 'absolute',
+                width: '100%',
+                height: '100%',
+                opacity: fadeAnim,
+              }}
+            />
+            <Animated.Text
+              style={{
+                fontSize: 60,
+                fontWeight: '700',
+                paddingTop: 300,
+                color: appColors.bleach,
+                textShadowColor: appColors.darkAccent,
+                textShadowOffset: {width: 5, height: 3},
+                textShadowRadius: 0,
+                opacity: fadeText,
+              }}>
+              AquaSafe
+            </Animated.Text>
           </View>
+          <View style={styles.textBlock}>
+            <Animated.View
+              style={{
+                opacity: fadeText,
+                paddingTop: 600,
+              }}>
+              <TouchableOpacity
+                style={styles.buttonBlock}
+                onPress={() => navigation.navigate('Registration')}>
+                <Text style={styles.textBlock}>Sign in</Text>
+              </TouchableOpacity>
+            </Animated.View>
+          </View>
+        </View>
       </>
-      );
-   }
+    );
+  };
 
-   const LoginScreen = () => {
-     return(
-       <Text>This stack is still being developed. Login is currently disabled</Text>
-     )
-   }
+  const LoginScreen = () => {
+    return (
+      <Text>
+        This stack is still being developed. Login is currently disabled
+      </Text>
+    );
+  };
 
-   return (
+  return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen //Welcome Screen
@@ -111,18 +121,16 @@ const App = () /*=> React$Node = ()*/ => {
         />
       </Stack.Navigator>
     </NavigationContainer>
-    );
+  );
 };
-
-
 
 const appColors = {
   background: '#85AEF1',
   darkAccent: '#525C82',
   titleDark: '#2E3652',
   titleBright: '#F5F7FF',
-  bleach: '#FFFFFF'
-}
+  bleach: '#FFFFFF',
+};
 
 const styles = StyleSheet.create({
   mainContainer: {
@@ -139,7 +147,7 @@ const styles = StyleSheet.create({
     left: 0,
     top: 0,
     width: '100%',
-    height: '100%'
+    height: '100%',
   },
   textBlock: {
     fontSize: 25,
@@ -160,7 +168,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
 
     color: appColors.bleach,
-  }
+  },
 });
 
 export default App;
